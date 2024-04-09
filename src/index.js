e3887171f3807b8e3ad9b15491c6cd34e042050e
Mohamed Ahmed <ma07041705@gmail.com>
2024-04-09 15:02:18 +0300

    jkkj

--- src/index.js
+++ src/index.js
@@ -1,74 +1,22 @@
-// Define the houses array
-let houses = [
-    {
-        prototype: "Villa",
-        sell: "for sale",
-        price: "$170,8978",
-        img: "https://media.licdn.com/dms/image/D4D12AQGPy6n4XA3eiQ/article-cover_image-shrink_720_1280/0/1687039862345?e=2147483647&v=beta&t=reyO4Xm8PwuFc3KlLdSGCxgassaMBixCPJhtcWASBaM",
-        description: "house in the small town",
-        location: "New York", 
-        sqft: "1000 Sqft",
-        bed: "3 Bed",
-        bath: "3 Bath"   
-    },
-    {
-        prototype: "Apartment",
-        sell: "for sale",
-        price: "$1,378845",
-        img: "https://corporatestays.com/wp-content/uploads/2022/02/The-Hazel-Amenities-Lounge-2.jpg",
-        description: "Golden Urban House For Sell",
-        location: "Las Vigas", 
-        sqft: "12,000 Sqft",
-        bed: "7 Bed",
-        bath: "9 Bath" 
-    },
-    {
-        prototype: "Apartment",
-        sell: "for sale",
-        price: "$1,0000",
-        img: "https://corporatestays.com/wp-content/uploads/2022/02/The-Hazel-Amenities-4-e1633774291295.jpg",
-        description: "community gated houses",
-        location: "LA", 
-        sqft: "5000 Sqft",
-        bed: "3 Bed",
-        bath: "2 Bath" 
-    },
-    {
-        prototype: "Houses",
-        sell: "for sale",
-        price: "$20,54345",
-        img: "https://assets-news.housing.com/news/wp-content/uploads/2022/03/31010142/Luxury-house-design-Top-10-tips-to-add-luxury-to-your-house-FEATURE-compressed.jpg",
-        description: "modern house For Sell",
-        location: "New York", 
-        sqft: "1000 Sqft",
-        bed: "1 Bed",
-        bath: "1 Bath" 
-    },
-    {
-        prototype: "Condominium",
-        sell: "for sale",
-        price: "$80,345",
-        img: "https://t4.ftcdn.net/jpg/01/69/69/21/360_F_169692156_L1aGrmJaHsZxF1sWQGuRKn3mR60bBqhN.jpg",
-        description: "century  house fully furnished",
-        location: "Washington", 
-        sqft: "15,000 Sqft",
-        bed: "6 Bed",
-        bath: "4 Bath"  
-    },
-    {
-        prototype: "House",
-        sell: "for sale",
-        price: "$966,4345",
-        img: "https://foyr.com/learn/wp-content/uploads/2019/03/Villa_Mistral-Singapore.jpg",
-        description: "modern house with a pool",
-        location: "New York", 
-        sqft: "1000 Sqft",
-        bed: "3 Bed",
-        bath: "2 Bath" 
-    },
-];
 
-// Define the createHouseCard function
+// API
+const houseApi = "https://propertyhub-mywm.onrender.com/houses";
+
+// DOM elements
+let houseContainer = document.getElementById("root")
+let houseFilter = document.getElementById("filter-houses")
+
+// fetch all houses
+const fetchData = () => {
+    return fetch(houseApi)
+      .then((res) => res.json())
+      .catch((error) => {
+        console.error("Error fetching houses:", error);
+      });
+  };
+  
+
+// card div for displaying houses 
 function createHouseCard(house) {
     return `
     <div class="property-item-container">
@@ -99,38 +47,51 @@ function createHouseCard(house) {
     `;
 }
 
-// Define the paintDevelopers function
-function paintDevelopers(houses) {
-    let cards = houses.map(house => createHouseCard(house)).join(" ");
-    document.getElementById("root").innerHTML = cards;
+
+// function for mapping the data
+function paintHouses() {
+    fetchData()
+    .then((houses) => {
+        let cards = houses.map(house => createHouseCard(house)).join(" ");
+        houseContainer.innerHTML = cards;
+      })
+      .catch((error) => {
+        console.error("Error fetching houses:", error);
+      });
 }
+paintHouses();
 
-// Paint all houses initially
-paintDevelopers(houses);
 
-document.getElementById("filter-houses").addEventListener("click", function() {
+houseFilter.addEventListener("click", function() {
     // Retrieve selected filter values
     const propertyType = document.getElementById("propertyType").value;
     const location = document.getElementById("location").value;
     const price = document.getElementById("price").value;
-
+    const sellingRenting = document.getElementById("sellingRenting").value;
 
     // Check if all filter values are set to '---select---'
-    if (propertyType === '---select---' && location === '---select---' && price === '---select---') {
+    if (propertyType === '---select---' && location === '---select---' && price === '---select---' && sellingRenting === '---select---') {
         alert("You haven't chosen or filtered any house.");
         return; // Exit the function without further processing
     }
 
-    // Filter houses based on selected values
-    const filteredHouses = houses.filter(house => {
-        return (house.prototype === propertyType) ||
-               ( house.location === location) ||
-               (parseFloat(house.price.replace('$', '').replace(',', '')) >= parseFloat(price)) 
+    fetchData()
+    .then((houses) => {
+        // Filter houses based on selected values
+        const filteredHouses = houses.filter(house => {
+            return (house.prototype === propertyType) ||
+                   (house.location === location) ||
+                   (house.houseFor === sellingRenting) ||
+                   (parseFloat(house.price.replace('$', '').replace(',', '')) >= parseFloat(price)) 
+        });
+        console.log('filter',filteredHouses); // Check filtered houses
+
+        // Paint filtered houses
+        paintHouses(filteredHouses);
+    })
+    .catch((error) => {
+        console.error("Error fetching houses:", error);
     });
-
-    console.log(filteredHouses); // Check filtered houses
-
-    // Paint filtered houses
-    paintDevelopers(filteredHouses);
 });
+
 
